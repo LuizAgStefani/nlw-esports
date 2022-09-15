000608c23202cec68b75e0a3e5a0dbfa8c79537a
Luiz <lastefani@cliptech.com.br>
2022-09-15 15:48:43 -0300

    Finished Stage 4 of Mobile

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -13,9 +13,9 @@ import { THEME } from "../../theme";
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  ads: any[];
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -25,10 +25,10 @@ interface Props extends TouchableOpacityProps {
 export function GameCard({ data, ...rest }: Props) {
   return (
     <TouchableOpacity style={styles.container} {...rest}>
-      <ImageBackground source={data.cover} style={styles.cover}>
+      <ImageBackground source={{ uri: data.bannerUrl }} style={styles.cover}>
         <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
-          <Text style={styles.name}>{data.name}</Text>
-          <Text style={styles.ads}>{data.ads} anúncios</Text>
+          <Text style={styles.name}>{data.title}</Text>
+          <Text style={styles.ads}>{data.ads.length} anúncio(s)</Text>
         </LinearGradient>
       </ImageBackground>
     </TouchableOpacity>
